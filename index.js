f5455e3f9aacef9dc4d699032c61eaaaaf393c00
Severo Iglesias <siglesias@ucol.mx>
2020-06-13 20:29:14 -0500

    prueba 1

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-
-
-    var lista = [0, 0, 247, 563, 224, 714, 0, 953, 708, 201, 887, 550, 515, 206, 131];
-    var n, i, k, aux;
-    n = lista.length;
-    console.log(lista); // Mostramos, por consola, la lista desordenada
-    // Algoritmo de burbuja
-    for (k = 1; k < n; k++) {
-        for (i = 0; i < (n - k); i++) {
-            if (lista[i] === 0) {
-                aux = lista[i];
-                lista[i] = lista[i + 1];
-                lista[i + 1] = aux;
-            }
+    var lista = [],
+        listaOrdenada = [];
+        
+        
+    document.getElementById("elementoOriginal").innerHTML=lista;
+    function Agregar() {
+        if(document.getElementById("valor").value!='')
+        
+        {
+            valor=document.getElementById("valor").value;
+            lista.unshift(valor);
+            document.getElementById("elementoOriginal").innerHTML=lista;
+           
+          
+        }
+        else
+        alert("No hay valor para agregar!!")
+        listaOrdenada = lista
+        }
+    function Ordenar() {
+        var n,
+            aux,
+            
+        n = listaOrdenada.length;
+        
+        for (let k = 1; k < n; k++) {
+            for (let i = 0; i < (n - k); i++) {
+                if (listaOrdenada[i] === 0) {
+                    aux = listaOrdenada[i];
+                    listaOrdenada[i] = listaOrdenada[i + 1];
+                    listaOrdenada[i + 1] = aux;
+                }
+                    
+            
         }
     }
+    document.getElementById("Ordenado").innerHTML=listaOrdenada;
+}
+function Limpiar() {
+    listaOrdenada = []
+    lista = []
+    document.getElementById("Ordenado").innerHTML=listaOrdenada;
+    document.getElementById("elementoOriginal").innerHTML=lista;  
+}
+console.log(listaOrdenada)
 
-    console.log(lista); // Mostramos, por consola, la lista ya ordenada
+    
+    
 
 
