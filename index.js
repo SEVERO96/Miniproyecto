7f165ca7302c5e8a8e1c2637db6c2d31c6f8f603
“[name]” <g5ada02@hackademy.mx>
2020-06-13 21:04:48 -0500

    Update

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
     var lista = [],
         listaOrdenada = [];
-        
-        
-    document.getElementById("elementoOriginal").innerHTML=lista;
+
+
+    document.getElementById("elementoOriginal").innerHTML = lista;
+
     function Agregar() {
-        if(document.getElementById("valor").value!='')
-        
+        if (document.getElementById("valor").value != '')
+
         {
-            valor=document.getElementById("valor").value;
+            valor = document.getElementById("valor").value;
             lista.unshift(valor);
-            document.getElementById("elementoOriginal").innerHTML=lista;
-           
-          
-        }
-        else
-        alert("No hay valor para agregar!!")
+            document.getElementById("elementoOriginal").innerHTML = lista;
+
+
+        } else
+            alert("No hay valor para agregar!!")
         listaOrdenada = lista
-        }
+    }
+
     function Ordenar() {
         var n,
             aux,
-            
-        n = listaOrdenada.length;
-        
+
+            n = listaOrdenada.length;
         for (let k = 1; k < n; k++) {
-            for (let i = 0; i < (n - k); i++) {
-                if (listaOrdenada[i] === 0) {
+            for (let i = 0; i < n - 1; i++) {
+                if (listaOrdenada[i] == 0) {
                     aux = listaOrdenada[i];
                     listaOrdenada[i] = listaOrdenada[i + 1];
                     listaOrdenada[i + 1] = aux;
                 }
-                    
-            
+
+
+            }
         }
+        document.getElementById("Ordenado").innerHTML = listaOrdenada;
+
     }
-    document.getElementById("Ordenado").innerHTML=listaOrdenada;
-}
-function Limpiar() {
-    listaOrdenada = []
-    lista = []
-    document.getElementById("Ordenado").innerHTML=listaOrdenada;
-    document.getElementById("elementoOriginal").innerHTML=lista;  
-}
-console.log(listaOrdenada)
 
-    
-    
-
-
+    function Limpiar() {
+        listaOrdenada = []
+        lista = []
+        document.getElementById("Ordenado").innerHTML = listaOrdenada;
+        document.getElementById("elementoOriginal").innerHTML = lista;
+    }
+    console.log(listaOrdenada)
